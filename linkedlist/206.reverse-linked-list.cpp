8429223cc45a5c1ca6dcab15c645af28c50ecbc2
Ziy1-Tan <ajb459684460@gmail.com>
2022-11-29 09:13:26 +0800

    linked list

---
+++ linkedlist/206.reverse-linked-list.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        if (head == nullptr)
+        {
+            return head;
+        }
+
+        ListNode *prev = nullptr, *curr = head;
+        while (curr->next != nullptr)
+        {
+            ListNode *next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        curr->next = prev;
+
+        return curr;
+    }
+};
+
+class Solution2
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        if (head == nullptr || head->next == nullptr)
+        {
+            return head;
+        }
+
+        ListNode *rev = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        return rev;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    auto print_list = [](ListNode *node) -> void
+    {
+        auto curr = node;
+        while (curr)
+        {
+            cout << curr->val << " ";
+            curr = curr->next;
+        }
+        cout << "\n";
+    };
+
+    ListNode *head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    Solution2 s;
+    auto res = s.reverseList(head);
+    print_list(res);
+    return 0;
+}
