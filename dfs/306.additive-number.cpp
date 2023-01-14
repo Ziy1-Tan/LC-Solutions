d8486194bb827c821d0f646a05dc75214a18641f
Ziy1-Tan <ajb459684460@gmail.com>
2023-01-14 11:10:20 +0800

    back t

---
+++ dfs/306.additive-number.cpp
@@ -0,0 +1,49 @@
+#include <deque>
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isAdditiveNumber(string num)
+    {
+        return dfs(num, 0, 0, 0, 0);
+    }
+
+private:
+    bool dfs(const string &num, int begin, int cnt, unsigned long long prev1, unsigned long long prev2)
+    {
+        if (begin >= num.size())
+            return cnt > 2;
+
+        unsigned long long curr = 0;
+        for (int i = begin; i < num.size(); i++)
+        {
+            if (num[begin] == '0' && i > begin)
+                return false;
+
+            curr = curr * 10 + (num[i] - '0');
+            if (cnt >= 2)
+            {
+                long sum = prev1 + prev2;
+                if (curr > sum)
+                    return false;
+                if (curr < sum)
+                    continue;
+            }
+
+            if (dfs(num, i + 1, cnt + 1, prev2, curr))
+                return true;
+        }
+
+        return false;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    auto res = (new Solution)->isAdditiveNumber("1023");
+    cout << res << endl;
+    return 0;
+}
