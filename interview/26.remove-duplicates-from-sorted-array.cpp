dd5f70434d9c72337a95bb778f4673ed6f1f4357
Ziy1-Tan <ajb459684460@gmail.com>
2023-02-20 21:49:49 +0800

    interview
    Signed-off-by: Ziy1-Tan <ajb459684460@gmail.com>

---
+++ interview/26.remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,22 @@
+#include <cassert>
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+  int removeDuplicates(vector<int> &nums) {
+    int slow = 0;
+    for (int fast = 0; fast < nums.size(); fast++) {
+      if (nums[fast] != nums[slow]) {
+        nums[++slow] = nums[fast];
+      }
+    }
+    return slow + 1;
+  }
+};
+
+int main(int argc, const char *argv[]) {
+  vector<int> nums{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+  assert((new Solution)->removeDuplicates(nums) == 5);
+  return 0;
+}
---
+++ interview/7.reverse-integer.cpp
@@ -0,0 +1,23 @@
+#include <climits>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+  int reverse(int x) {
+    int res = 0;
+    while (x != 0) {
+      int digit = x % 10;
+      if (res > INT_MAX / 10 || res < INT_MIN / 10)
+        return 0;
+      res = res * 10 + digit;
+      x /= 10;
+    }
+    return res;
+  }
+};
+
+int main(int argc, const char *argv[]) {
+  cout << (new Solution)->reverse(123) << endl;
+  return 0;
+}
